5d3978bb4323840e7d361fee15638ac6c09ee672
AnkitM <ankit.malvi@gmail.com>
2021-07-13 15:37:02 +0530

    BranchA changes

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,8 @@ public class Test {
 		System.out.println("Hi");
 		System.out.println("Added in Branch 1");
 		System.out.println("Added on second stash ");
+		System.out.println("Branch A changes 1");
+		System.out.println("Branch A changes 2");
+		System.out.println("Branch A changes 3");
 	}
 }
