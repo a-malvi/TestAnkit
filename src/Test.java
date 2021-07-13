47321b14ef42b3089c97b62a007e1c9072d8f326
AnkitM <ankit.malvi@gmail.com>
2021-07-13 08:01:36 +0530

    Updates

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 
 public class Test {
-public static void main(String[] args) {
-	System.out.println("Hi");
-	System.out.println("Added in Branch 1");
-}
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Added in Branch 1");
+		System.out.println("Added on second stash ");
+	}
 }
